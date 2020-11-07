6f43720da10341cd2c0dfcb90416ea10077d4244
Reka Ehreth <ehreth.reka@gmail.com>
2020-11-08 00:00:08 +0100

    Final version ^_^

--- TextReader/Statistics.cs
+++ TextReader/Statistics.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace TextReader
 {
-    class Statistics
+    public class Statistics
     {
         public string FileContent { get; private set; }     // auto property, private variable is automatically generated, can only be accessed by the property
         public Dictionary<string, int> WordCount { get; private set; }
@@ -23,11 +23,11 @@ namespace TextReader
             words = words.Where(s => s.Length > 0).ToArray();
             for(int i = 0; i < words.Length; ++i)
             {
-                while (words[i].Length > 0 && !Char.IsLetter(words[i][0]))
+                while (words[i].Length > 0 && !char.IsLetter(words[i][0]))
                 {
                     words[i] = words[i].Remove(0, 1);
                 }
-                while (words[i].Length > 0 && !Char.IsLetter(words[i][-1]))
+                while (words[i].Length > 0 && !char.IsLetter(words[i][^1]))
                 {
                     words[i] = words[i].Remove(words[i].Length - 1, 1);
                 }
@@ -39,27 +39,5 @@ namespace TextReader
                     WordCount.Add(words[i], 1);
             }
         }
-
-        // A Main-ben ezután hívjuk meg CountWords függvényt. 
-        // A Dictionary egy rendezetlen típus, ezért a kiírás elott rendeznünk kell az adatokat az elofordulások száma szerint. 
-        // Rendezzük az értékek szerint WordCount-ot az OrderByDescending() metódussal, amelynek a rendezés feltételét egy lambdakifejezésként
-        // adjuk át, majd az eredmény IEnumerable-t mentsük el egy lokális változóba.
-        // A rendezést LINQ lekérdezéssel, többféle szintaxissal is végezhetjük.
-
-        //1) Method Syntax:
-        //var pairs = statistics.WordCount.OrderByDescending(p => p.Value);
-
-        //2) Query Syntax:
-        //var pairs = from pair in WordCount
-        //            orderby pair.Value descending
-        //            select pair;
-
-        //A két módszer ugyanazt az eredményt adja. Ezután a pairs-en végigiterálhatunk
-        //egy foreach ciklussal, hogy kiírjuk a kulcs-érték párokat.
-
-        //foreach (var pair in pairs)
-        //{
-        //    Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
-        //}
     }
 }
---
+++ VisualizeWordCounter/WordCountDialog.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace VisualizeWordCounter
+{
+    partial class WordCountDialog
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialogMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.countWordsFileDialog = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.listBoxCounter = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileMenu
+            // 
+            this.fileMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileDialogMenuItem,
+            this.countWordsFileDialog});
+            this.fileMenu.Name = "fileMenu";
+            this.fileMenu.Size = new System.Drawing.Size(46, 24);
+            this.fileMenu.Text = "File";
+            // 
+            // openFileDialogMenuItem
+            // 
+            this.openFileDialogMenuItem.Name = "openFileDialogMenuItem";
+            this.openFileDialogMenuItem.Size = new System.Drawing.Size(175, 26);
+            this.openFileDialogMenuItem.Text = "Open file";
+            // 
+            // countWordsFileDialog
+            // 
+            this.countWordsFileDialog.Name = "countWordsFileDialog";
+            this.countWordsFileDialog.Size = new System.Drawing.Size(175, 26);
+            this.countWordsFileDialog.Text = "Count words";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(13, 46);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(390, 424);
+            this.textBox.TabIndex = 1;
+            // 
+            // listBoxCounter
+            // 
+            this.listBoxCounter.FormattingEnabled = true;
+            this.listBoxCounter.ItemHeight = 20;
+            this.listBoxCounter.Location = new System.Drawing.Point(409, 46);
+            this.listBoxCounter.Name = "listBoxCounter";
+            this.listBoxCounter.Size = new System.Drawing.Size(379, 424);
+            this.listBoxCounter.TabIndex = 2;
+            // 
+            // WordCountDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 483);
+            this.Controls.Add(this.listBoxCounter);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "WordCountDialog";
+            this.Text = "Word counter";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileMenu;
+        private System.Windows.Forms.ToolStripMenuItem openFileDialogMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem countWordsFileDialog;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ListBox listBoxCounter;
+    }
+}
+
